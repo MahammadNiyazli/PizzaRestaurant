11476ecfe106ea397fc43b55f83fb42ffa716df9
MahammadNiyazli <mahammad.niyazli@gmail.com>
2021-08-17 21:21:24 +0400

    changes

---
+++ PizzaDbApp/src/main/java/com/company/PizzaDbAppApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaDbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaDbAppApplication.class, args);
+	}
+
+}
